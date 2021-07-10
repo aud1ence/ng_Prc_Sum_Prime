3829139527e13ac5aba70a2939c5b943560cd4e1
aud1ence <daruma19051890@gmail.com>
2021-07-10 12:58:44 +0700

    done

---
+++ src/main.js
@@ -0,0 +1,32 @@
+"use strict";
+exports.__esModule = true;
+var core_1 = require("@angular/core");
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var app_module_1 = require("./app/app.module");
+var environment_1 = require("./environments/environment");
+if (environment_1.environment.production) {
+    core_1.enableProdMode();
+}
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule)["catch"](function (err) { return console.error(err); });
+var sum = 0;
+var count = 0;
+for (var i = 2; count < 30; i++) {
+    var isPrime = true;
+    if (i == 2) {
+        sum += i;
+        count++;
+        continue;
+    }
+    for (var j = 2; j <= Math.sqrt(i); j++) {
+        if (i % j == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (!isPrime) {
+        continue;
+    }
+    sum += i;
+    count++;
+}
+console.log(sum);
